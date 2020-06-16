bb1f5c76105f7de69aba399d1ad31ffdc03d4035
Torean Joel <torean@trixta.com>
2020-06-17 00:54:48 +0200

    add imports for external libraries

--- imports.js
+++ imports.js
@@ -1,3 +1,6 @@
+//import libraries
+includeJs('./libraries/gsap/minified/gsap.min.js');
+includeJs('./libraries/gsap/minified/ScrollTrigger.min.js');
 // import the scene scripts from the scene folder
 includeJs('./scenes/scene_1.js');
-includeJs('./scenes/scene_2.js')
+includeJs('./scenes/scene_2.js');
